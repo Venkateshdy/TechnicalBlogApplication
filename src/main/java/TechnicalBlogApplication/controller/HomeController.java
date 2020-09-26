a91a3a185c358818e63ae4e51007588d8a943e45
unknown <venkatesh>
2020-09-26 19:09:48 +0530

    First milestone

---
+++ src/main/java/TechnicalBlogApplication/controller/HomeController.java
@@ -0,0 +1,44 @@
+package TechnicalBlogApplication.controller;
+
+import TechnicalBlogApplication.Model.Post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping ("/")
+
+    public String getAllPostS(Model model){
+
+        ArrayList <Post> posts = new ArrayList<> ();
+        Post post1 = new Post();
+        post1.setTitle ("post1");
+        post1.setBody ("post body1");
+        post1.setDate (new Date());
+
+        Post post2 = new Post();
+        post2.setTitle ("post2");
+        post2.setBody ("post body2");
+        post2.setDate (new Date());
+
+        Post post3 = new Post();
+        post3.setTitle ("post3");
+        post3.setBody ("post body3");
+        post3.setDate (new Date());
+
+        posts.add (post1);
+        posts.add (post2);
+        posts.add (post3);
+
+        model.addAttribute ("posts", posts);
+            return "index";
+    }
+}
